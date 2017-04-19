b8cdfaa8c1df358969fc08e69ada47a9299ae30c
tangerwei <tangerwei@foxmail.com>
2017-04-19 17:57:15 +0800

    update

---
+++ move131-build.bat
@@ -0,0 +1,2 @@
+xcopy C:\Users\erwei.tang\project-using-gulp\build\sass\demo.css \\172.16.20.131\aqua\webapps\demo_tew\test\sass /s /h /d /y
+xcopy C:\Users\erwei.tang\project-using-gulp\build\index.html \\172.16.20.131\aqua\webapps\demo_tew\test /s /h /d /y
